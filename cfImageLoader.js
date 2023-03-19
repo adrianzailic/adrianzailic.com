c71fa54954af357159603419213f7e3156fc8fb5
Adrian Zailic <adrianzailic@gmail.com>
2023-03-19 13:38:05 +0200

    Corrected domain

--- cfImageLoader.js
+++ cfImageLoader.js
@@ -1,5 +1,5 @@
 // cfImageLoader.js
 export default function cfImageLoader({ src, width, quality }) {
     const params = [`width=${width}`, `quality=${quality || 75}`, 'format=auto']
-    return `https://cfnext.designly.biz/cdn-cgi/image/${params.join(',')}/${src}`
+    return `https://adrianzailic.com/cdn-cgi/image/${params.join(',')}/${src}`
 }
