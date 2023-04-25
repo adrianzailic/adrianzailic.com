a60b320d0d504655d00b8f6a02cb8124904db4bf
Adrian Zailic <adrianzailic@gmail.com>
2023-04-25 22:25:33 +0300

    Improved responsive design

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,14 +5,16 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="py-10 flex flex-col lg:flex-row items-center">
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
+        <div className="py-10 flex flex-col lg:flex-row items-center space-y-2">
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/6">
             <a
               href={`https://www.linkedin.com/in/adrianzailic/`}
               className="mx-3 font-bold hover:underline"
             >
               LinkedIn
             </a>
+          </div>
+          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/6">
             <a
               href={`https://www.instagram.com/adrianzailic/`}
               className="mx-3 font-bold hover:underline"
