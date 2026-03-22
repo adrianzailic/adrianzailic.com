0d0d6c6b8fd5c76c344731be2b9f3d93c8840785
Adrian Zailic <adrianzailic@gmail.com>
2026-03-22 14:29:33 +0200

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), sitemap()],
   build: {
     format: 'directory',
   },
